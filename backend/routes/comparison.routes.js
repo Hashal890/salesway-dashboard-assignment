ceeff850b2206d9b9447c90a3ed9c520f9762ce3
amolpradeshi <pardeshiharshal90@gmail.com>
2024-07-18 03:33:49 +0530

    created api for comparisons

--- backend/routes/comparison.routes.js
+++ backend/routes/comparison.routes.js
@@ -1,5 +1,26 @@
 const express = require("express");
+const ComparisonModel = require("../models/comparison.model");
 
 const comparisonRouter = express.Router();
+
+comparisonRouter.get("/", async (req, res) => {
+  try {
+    const comparisonData = await ComparisonModel.find();
+    res.status(200).json({ message: "Found all comparison data!", comparisonData });
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+});
+
+comparisonRouter.post("/", async (req, res) => {
+  try {
+    const newComparison = req.body;
+    const comparison = new ComparisonModel(newComparison);
+    await comparison.save();
+    res.status(200).json({ message: "Comparison addded!", newComparison });
+  } catch (error) {
+    res.status(400).json({ message: error });
+  }
+});
 
 module.exports = comparisonRouter;
