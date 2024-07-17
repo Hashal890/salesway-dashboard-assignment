7e1503f4d4eb6d6cc862d5cac00e402283e0ae7f
amolpradeshi <pardeshiharshal90@gmail.com>
2024-07-18 03:47:11 +0530

    create route for top-product

--- backend/routes/top-products.routes.js
+++ backend/routes/top-products.routes.js
@@ -1,5 +1,28 @@
 const express = require("express");
+const ProductModel = require("../models/top-products.model");
 
 const topProductsRouter = express.Router();
+
+topProductsRouter.get("/", async (req, res) => {
+  try {
+    const topProductsData = await ProductModel.find();
+    res
+      .status(200)
+      .json({ message: "Found all top products data!", topProductsData });
+  } catch (error) {
+    res.status(400).json({ message: error });
+  }
+});
+
+topProductsRouter.post("/", async (req, res) => {
+  try {
+    const newTopProduct = req.body;
+    const newTopProductDatabaseUpload = new ProductModel(newTopProduct);
+    await newTopProductDatabaseUpload.save();
+    res.status(200).json({ message: "Top product addded!", newTopProduct });
+  } catch (error) {
+    res.status(400).json({ message: error });
+  }
+});
 
 module.exports = topProductsRouter;
